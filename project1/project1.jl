2e230e2b4f0102f788c973005cff3b1be24d95f2
Talia Blum <taliab@stanford.edu>
2024-10-17 16:33:49 -0700

    mvp

--- project1/project1.jl
+++ project1/project1.jl
@@ -1,5 +1,12 @@
 using Graphs
 using Printf
+using CSV
+using DataFrames
+using Random
+using SpecialFunctions
+using LinearAlgebra
+using GraphPlot
+using Compose
 
 """
     write_gph(dag::DiGraph, idx2names, filename)
@@ -14,12 +21,109 @@ function write_gph(dag::DiGraph, idx2names, filename)
     end
 end
 
+function sub2ind(siz, x)
+    k = vcat(1, cumprod(siz[1:end-1]))
+    return dot(k, x .- 1) + 1
+end
+
+struct Variable 
+    name::Symbol
+    r::Int # number of possible values
+end
+
+function prior(vars, G)
+    n = length(vars)
+    r = [vars[i].r for i in 1:n]
+    q = [prod([r[j] for j in inneighbors(G,i)]) for i in 1:n]
+    return [ones(q[i], r[i]) for i in 1:n]
+end
+    
+function statistics(vars, G, D::Matrix{Int})
+    n = size(D, 1)
+    r = [vars[i].r for i in 1:n]
+    q = [prod([r[j] for j in inneighbors(G,i)]) for i in 1:n]
+    M = [zeros(q[i], r[i]) for i in 1:n]
+    for o in eachcol(D)
+        for i in 1:n k = o[i]
+            parents = inneighbors(G,i)
+            j=1
+            if !isempty(parents)
+                j = sub2ind(r[parents], o[parents])
+            end
+            M[i][j,k] += 1.0 
+        end
+    end
+    return M
+end
+
+function bayesian_score_component(M, alpha)
+    p = sum(loggamma.(alpha + M))
+    p -= sum(loggamma.(alpha))
+    p += sum(loggamma.(sum(alpha,dims=2)))
+    p -= sum(loggamma.(sum(alpha,dims=2) + sum(M,dims=2)))
+    return p
+end
+
+function bayesian_score(vars, G, D) 
+    n = length(vars)
+    M = statistics(vars, G, D)
+    alpha = prior(vars, G)
+    return sum(bayesian_score_component(M[i], alpha[i]) for i in 1:n)
+end
+
+struct K2Search
+    ordering::Vector{Int} # variable ordering
+end
+
+function fit(method::K2Search, vars, D)
+    G = SimpleDiGraph(length(vars))
+    for (k,i) in enumerate(method.ordering[2:end])
+        y = bayesian_score(vars, G, D) 
+        while true
+            y_best, j_best = -Inf, 0
+            for j in method.ordering[1:k]
+                if !has_edge(G, j, i)
+                    add_edge!(G, j, i)
+                    y_prime = bayesian_score(vars, G, D) 
+                    if y_prime > y_best
+                        y_best, j_best = y_prime, j
+                    end
+                    rem_edge!(G, j, i)
+                end
+            end
+            if y_best > y 
+                y = y_best
+                add_edge!(G, j_best, i)
+            else
+                break
+            end
+        end
+    end
+    return G
+end
 
 function compute(infile, outfile)
+
 
     # WRITE YOUR CODE HERE
     # FEEL FREE TO CHANGE ANYTHING ANYWHERE IN THE CODE
     # THIS INCLUDES CHANGING THE FUNCTION NAMES, MAKING THE CODE MODULAR, BASICALLY ANYTHING
+
+    #todo: turn infile into vars, G, D, ie compute graph structure
+    Data = CSV.read(infile, DataFrame)
+    varinfo = describe(Data,:max)
+    vars = [Variable(varinfo.variable[i], varinfo.max[i]) for i in 1:length(varinfo.variable)]
+
+    D = Matrix(transpose(Matrix(Data)))
+
+    nvars = length(vars)
+    ordering = shuffle(collect(1:nvars))
+    method = K2Search(ordering)
+
+    G = fit(method, vars, D)
+    println(G)
+    #compute graph like above
+    # todo: turn outfile into outfile
 
 end
 
